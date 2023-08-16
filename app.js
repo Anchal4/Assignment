c9739b1b02295942b1bcf276bed09265d562b978
Anchal sahu <anchal3444@gmail.com>
2023-08-16 16:58:39 +0530

    no changes

---
+++ app.js
@@ -0,0 +1,9 @@
+
+var username = 'raju'
+let age = 20
+const city = 'jaipur'
+var ismarried = false
+
+console.log( "my name is" + username)
+
+console.log("my name is" + username"|" ,"my age is" +age"|")
